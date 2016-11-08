35ed40ac133091a0e17cc94c9f634cc6229d7cb5
John Rineck <jrineck@users.noreply.github.com>
2016-11-07 22:11:08 -0600

    Create framework.php

---
+++ Site/framework.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     JSNExtension
+ * @subpackage  TPLFramework
+ * @author      John Rineck <support@rineck.org>
+ * @copyright   Copyright (C) 2016 Rineck.org. All Rights Reserved.
+ * 
+ * Websites:    https://www.rineck.org
+ * Support:     https://www.rineck.org/support
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Dummy class for TPLFramework plugin
+ * 
+ * @package     TPLFramework
+ * @subpackage  Plugin
+ * @since       1.0.0
+ */
+class PlgSystemJSNTPLFramework
+{
+
+}

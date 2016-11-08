cad7fa180b611602ad157822b771fd92f793b11e
John Rineck <jrineck@users.noreply.github.com>
2016-11-07 22:29:23 -0600

    Create component.php

---
+++ Site/Template/component.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author    John Rineck https://rineck.org
+ * @copyright Copyright (C) 2016 Rineck.org. All rights reserved.
+ */
+
+// No direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Load template framework
+if (!defined('JSN_PATH_TPLFRAMEWORK')) {
+	require_once JPATH_ROOT . '/plugins/system/jsntplframework/jsntplframework.defines.php';
+	require_once JPATH_ROOT . '/plugins/system/jsntplframework/libraries/joomlashine/loader.php';
+}
+
+// Preparing template parameters
+JSNTplTemplateHelper::prepare(false, false);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+	<jdoc:include type="head" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+</head>
+<body class="contentpane" id="jsn-content-popup">
+	<jdoc:include type="message" />
+	<jdoc:include type="component" />
+</body>
+</html>
